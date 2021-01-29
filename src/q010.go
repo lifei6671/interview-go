400b9f8f17fcd0fc819bbc2d50fdf0e5ef6498cb
323364 <syj241225@ncarzone.com>
2021-01-29 11:40:06 +0800

    Bugfix: 问题10：延迟并发map,并发写时panic修复

---
+++ src/q010.go
@@ -0,0 +1,102 @@
+/*
+@Time : 2021/1/29 上午9:31
+@Author : sunyujia
+@File : q010
+@Software: GoLand
+*/
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type sp interface {
+	Out(key string, val interface{})  //存入key /val，如果该key读取的goroutine挂起，则唤醒。此方法不会阻塞，时刻都可以立即执行并返回
+	Rd(key string, timeout time.Duration) interface{}  //读取一个key，如果key不存在阻塞，等待key存在或者超时
+}
+
+type Map struct {
+	c   map[string]*entry
+	rmx *sync.RWMutex
+}
+type entry struct {
+	ch      chan struct{}
+	value   interface{}
+	isExist bool
+}
+
+func (m *Map) Out(key string, val interface{}) {
+	m.rmx.Lock()
+	defer m.rmx.Unlock()
+	item, ok := m.c[key]
+	if !ok {
+		m.c[key] = &entry{
+			value: val,
+			isExist: true,
+		}
+		return
+	}
+	item.value = val
+	if !item.isExist {
+		if item.ch != nil {
+			close(item.ch)
+			item.ch = nil
+		}
+	}
+	return
+}
+
+func (m *Map) Rd(key string, timeout time.Duration) interface{} {
+	m.rmx.Lock()
+	if e, ok := m.c[key]; ok && e.isExist {
+		m.rmx.Unlock()
+		return e.value
+	} else if !ok {
+		e = &entry{ch: make(chan struct{}), isExist: false}
+		m.c[key] = e
+		m.rmx.Unlock()
+		fmt.Println("协程阻塞 -> ", key)
+		select {
+		case <-e.ch:
+			return e.value
+		case <-time.After(timeout):
+			fmt.Println("协程超时 -> ", key)
+			return nil
+		}
+	} else {
+		m.rmx.Unlock()
+		fmt.Println("协程阻塞 -> ", key)
+		select {
+		case <-e.ch:
+			return e.value
+		case <-time.After(timeout):
+			fmt.Println("协程超时 -> ", key)
+			return nil
+		}
+	}
+}
+
+func main() {
+	mapval := Map{
+		c:   make(map[string]*entry),
+		rmx: &sync.RWMutex{},
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			val := mapval.Rd("key", time.Second * 6)
+			fmt.Println(val)
+		}()
+	}
+
+	time.Sleep(time.Second * 3)
+	for i := 0; i < 10; i ++ {
+		go func(val int) {
+			mapval.Out("key", val)
+		}(i)
+	}
+
+	time.Sleep(time.Second * 30)
+}
